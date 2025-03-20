b87b5681881fb5efe717350e57face182532b5c5
mrCr4b <yueru6221@proton.me>
2025-03-20 22:35:36 +0700

    'View seats for admin'

--- obj/Debug/net9.0/Movie.AssemblyInfo.cs
+++ obj/Debug/net9.0/Movie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Movie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+995ff197b0cc48762acc163afd89d36fd6c93c0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1c6328325b4ff8317d52723722ca20410c352d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Movie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Movie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
